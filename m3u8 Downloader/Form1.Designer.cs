9450d9095a619e9e3b4527eff11f508d5b2336a4
Emmanouil Kalyvas <emkalyvas@mnapps.net>
2021-01-27 12:13:32 +0200

    Add project files.

---
+++ m3u8 Downloader/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace m3u8_Downloader
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_link = new System.Windows.Forms.TextBox();
+            this.label_link = new System.Windows.Forms.Label();
+            this.button_download = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.ListBox();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input_link
+            // 
+            this.input_link.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.input_link.Location = new System.Drawing.Point(56, 12);
+            this.input_link.Name = "input_link";
+            this.input_link.Size = new System.Drawing.Size(367, 26);
+            this.input_link.TabIndex = 0;
+            // 
+            // label_link
+            // 
+            this.label_link.AutoSize = true;
+            this.label_link.Location = new System.Drawing.Point(12, 15);
+            this.label_link.Name = "label_link";
+            this.label_link.Size = new System.Drawing.Size(38, 20);
+            this.label_link.TabIndex = 1;
+            this.label_link.Text = "Link";
+            // 
+            // button_download
+            // 
+            this.button_download.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_download.Location = new System.Drawing.Point(16, 44);
+            this.button_download.Name = "button_download";
+            this.button_download.Size = new System.Drawing.Size(407, 38);
+            this.button_download.TabIndex = 2;
+            this.button_download.Text = "Download";
+            this.button_download.UseVisualStyleBackColor = true;
+            this.button_download.Click += new System.EventHandler(this.button_download_Click);
+            // 
+            // list
+            // 
+            this.list.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 20;
+            this.list.Location = new System.Drawing.Point(16, 88);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(407, 324);
+            this.list.TabIndex = 3;
+            // 
+            // status
+            // 
+            this.status.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(12, 421);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 20);
+            this.status.TabIndex = 4;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 450);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.button_download);
+            this.Controls.Add(this.label_link);
+            this.Controls.Add(this.input_link);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(461, 510);
+            this.MinimumSize = new System.Drawing.Size(461, 510);
+            this.Name = "main";
+            this.Text = "m3u8 Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input_link;
+        private System.Windows.Forms.Label label_link;
+        private System.Windows.Forms.Button button_download;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ m3u8 Downloader/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace m3u8_Downloader
+{
+    public partial class main : Form
+    {
+        //Constants :
+        const string CO_VIDEO_NAME_TS = "video.ts";
+        const string CO_VIDEO_NAME_MP4 = "video.mp4";
+        const string CO_WORKING_FOLDER_NAME = "job_";
+        const string CO_M3U8_STARTS_WITH = "#EXTM3U";
+        const string CO_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
+
+        //Status texts :
+        private static string STATUS_DECODING_M3U8 = "Decoding m3u8 file...";
+        private static string STATUS_DOWNLOADING = "Downloading video parts...";
+        private static string STATUS_COMBINING_PARTS = "Combining parts into one...";
+        private static string STATUS_COMPLETED = "Done";
+
+        //Error texts :
+        private static string ERROR_DOWNLOAD_FAILED = "Failed to download file (*)";
+        private static string ERROR_INVALID_URL = "Invalid URL";
+
+        public main()
+        {
+            InitializeComponent();
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            ServicePointManager.DefaultConnectionLimit = 9999;
+        }
+
+        private void button_download_Click(object sender, EventArgs e)
+        {
+            if (input_link.Text == "") return;
+
+            button_download.Enabled = false;
+            status.Text = STATUS_DECODING_M3U8;
+            
+            string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            byte[] m3u8_bytes = null;
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    client.Headers.Add(HttpRequestHeader.UserAgent, CO_USER_AGENT);
+                    m3u8_bytes = client.DownloadData(input_link.Text);
+                }
+                catch (Exception eror) {
+                    MessageBox.Show(ERROR_DOWNLOAD_FAILED.Replace("*", eror.Message));
+                    button_download.Enabled = true;
+                    changeStatus(this,"");
+                    return;
+                }
+            }
+            MemoryStream m3u8MemoryStream = new MemoryStream(m3u8_bytes);
+            StreamReader m3u8 = new StreamReader(m3u8MemoryStream); //new StreamReader(path + @"\" + tmp_filename);
+            if (!m3u8.ReadLine().StartsWith(CO_M3U8_STARTS_WITH))
+            {
+                MessageBox.Show(ERROR_INVALID_URL);
+                m3u8.Close();
+                m3u8.Dispose();
+                button_download.Enabled = true;
+                changeStatus(this, "");
+                return;
+            }
+            List<string> urls = new List<string>();
+            string baseURL = input_link.Text.Substring(0, input_link.Text.Length - input_link.Text.Split('/').GetValue(input_link.Text.Split('/').Length - 1).ToString().Length );
+            string line = "";
+            while ((line = m3u8.ReadLine()) != null)
+            {
+                if (line.StartsWith("#")) continue;
+                urls.Add(baseURL + line);
+            }
+            m3u8.Close();
+            m3u8.Dispose();
+
+            Thread download = new Thread(() => downloadFiles(this,urls));
+            download.Start();
+        }
+
+        private void downloadFiles(main control, List<string> urls)
+        {
+            changeStatus(control, STATUS_DOWNLOADING);
+            string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string folder = "job_" + (new Random(Guid.NewGuid().GetHashCode() + 123)).Next(0, 10000000);
+            
+            if (Directory.Exists(path + @"\" + folder)) Directory.Delete(path + @"\" + folder, true);
+            Directory.CreateDirectory(path + @"\" + folder);
+            path = path + @"\" + folder + @"\";
+            
+            int i = 0;
+            bool tryagain = true;
+            int urlNumber = urls.Count();
+            MemoryStream videoTS = new MemoryStream();
+            using (var client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.UserAgent, CO_USER_AGENT);
+                foreach (string url in urls)
+                {
+                    tryagain = true;
+                    while (tryagain)
+                    {
+                        tryagain = false;
+                        try
+                        {
+                            byte[] file = client.DownloadData(url);
+                            videoTS.Write(file, 0, file.Length);
+                        }
+                        catch
+                        {
+                            if (File.Exists(path + i.ToString() + ".ts")) File.Delete(path + i.ToString() + ".ts");
+                            Thread.Sleep(5000);
+                            tryagain = true;
+                        }
+                    }
+                    Thread.Sleep(50);
+                    control.list.Invoke((MethodInvoker)delegate
+                    {
+                        list.Items.Add(i.ToString() + ".ts - Downloaded");
+                        status.Text = STATUS_DOWNLOADING + " (" + i.ToString() + "/" + urlNumber + ")...";
+                        list.SelectedIndex = list.Items.Count - 1;
+                    });
+                    i++;
+                }
+            }
+            changeStatus(control, STATUS_COMBINING_PARTS);
+            Thread.Sleep(100);
+
+            File.WriteAllBytes(path + CO_VIDEO_NAME_TS, videoTS.ToArray());
+            videoTS.Close();
+            videoTS.Dispose();
+
+            Process ffmpeg = new Process();
+            ffmpeg.StartInfo.FileName = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\ffmpeg.exe";
+            ffmpeg.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            ffmpeg.StartInfo.Arguments = "-i \"" + path + CO_VIDEO_NAME_TS + "\" -acodec copy -vcodec copy \"" + path + CO_VIDEO_NAME_MP4 + "\"";
+            ffmpeg.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            ffmpeg.Start();
+            ffmpeg.WaitForExit();
+
+            File.Delete(path + CO_VIDEO_NAME_TS);
+
+            Thread.Sleep(100);
+            changeStatus(control, STATUS_COMPLETED);
+            Process.Start(path);
+
+            control.button_download.Invoke((MethodInvoker)delegate
+            {
+                button_download.Enabled = true;
+                input_link.Text = "";
+                list.Items.Clear();
+            });
+        }
+
+        private void changeStatus(main control ,string text)
+        {
+            control.status.Invoke((MethodInvoker)delegate
+            {
+                control.status.Text = text;
+            });
+        }
+    }
+}
